ed0de04592e211c2a71cc653dcbcf177445ce155
VinAhmad <kev.af18@gmail.com>
2023-11-16 10:35:01 +0700

    test1

--- database/migrations/2023_11_10_132726_create_master_kategori_table.php
+++ database/migrations/2023_11_10_132726_create_master_kategori_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('master_kategori', function (Blueprint $table) {
             $table->id();
-            $table->string('nama');
+            $table->string('nama', 20);
             $table->
             $table->timestamps();
         });
